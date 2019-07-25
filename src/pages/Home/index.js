9864b6f0e6ff1aac892177d737fc25ae424739c5
Renato <renatomarquesteles@gmail.com>
2019-07-25 02:51:45 -0300

    feature: Estilizacao da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,123 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/w_600,h_600,f_auto,q_auto:sensitive,fl_lossy/175b377ca83441278f39a7fe00a64e74_9366/Tenis_Adiease_Preto_BY4028_01_standard.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/w_600,h_600,f_auto,q_auto:sensitive,fl_lossy/175b377ca83441278f39a7fe00a64e74_9366/Tenis_Adiease_Preto_BY4028_01_standard.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/w_600,h_600,f_auto,q_auto:sensitive,fl_lossy/175b377ca83441278f39a7fe00a64e74_9366/Tenis_Adiease_Preto_BY4028_01_standard.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/w_600,h_600,f_auto,q_auto:sensitive,fl_lossy/175b377ca83441278f39a7fe00a64e74_9366/Tenis_Adiease_Preto_BY4028_01_standard.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/w_600,h_600,f_auto,q_auto:sensitive,fl_lossy/175b377ca83441278f39a7fe00a64e74_9366/Tenis_Adiease_Preto_BY4028_01_standard.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/w_600,h_600,f_auto,q_auto:sensitive,fl_lossy/175b377ca83441278f39a7fe00a64e74_9366/Tenis_Adiease_Preto_BY4028_01_standard.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/w_600,h_600,f_auto,q_auto:sensitive,fl_lossy/175b377ca83441278f39a7fe00a64e74_9366/Tenis_Adiease_Preto_BY4028_01_standard.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
